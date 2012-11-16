07b5890e063d3c7470e304a91752cd1c7007cc34
Pablo Duboue <pablo.duboue@gmail.com>
2012-11-15 19:55:17 -0500

    Merged sms and phone into pbx

--- components/sms/skeleton/fts_sms/fts_sms/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-   url(r'^sms/send$', 'fts_sms.views.stub', name='sms_send'),
-     # POST user_id, message_type, message_data
-)
--- components/sms/skeleton/fts_sms/fts_sms/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import Http404, HttpResponseRedirect,HttpResponseForbidden, HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template.context import RequestContext
-
-def stub(request,**kwargs):
-    return HttpResponse('<h1>Not implemented</h1>')
